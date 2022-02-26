0d8b74d55c6d18813d1e072355033c1d09ebfcd8
vincesaykao <vtsaykao@hotmail.com>
2022-02-26 10:11:19 -0600

    got GET to work

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -3,25 +3,25 @@
 
 export default function Feeling() {
 
-    // function getFeeling() {
-    //     console.log('Getting....Feels...');
-    //     axios({
-    //         method: 'GET',
-    //         url: '/',
-    //     })
-    //         .then((response) => {
-    //             console.log(respose.data)
+    function getFeeling() {
+        console.log('Getting....Feels...');
+        axios({
+            method: 'GET',
+            url: '/api/feedback',
+        })
+            .then((response) => {
+                console.log(respose.data)
 
-    //             dispatchEvent({
-    //                 type: 'GET_FEELING',
-    //                 payload: response.data,
-    //             });
-    //         })
-    //         .catch((error) => {
-    //             console.log('error', error);
-    //         })
+                dispatchEvent({
+                    type: 'GET_FEELING',
+                    payload: response.data,
+                });
+            })
+            .catch((error) => {
+                console.log('error', error);
+            })
 
-    // }
+    }
 
     return (
         <>
